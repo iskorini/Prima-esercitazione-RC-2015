c3dd0acf9770b1bcc3331cf580b7c0904cf5699f
iskorini <iskorini@gmail.com>
2015-11-01 16:23:38 +0100

    Implemented new function

--- server.py
+++ server.py
@@ -1,22 +1,26 @@
 import socket
+import os
 class MyFtpServer:
 
 	def __init__ (self, userdata, port):
 		self.__userdata = userdata
 		self.__port = port
 		self.__userConnected = 'EMPTY'
-		self.__pending_request = 0
+		self.__currentPath = ''
+		self.__pending_login = 0
 
+	def is_logged(self):
+		return (self.__userConnected != 'EMPTY' and self.__pending_login == 0)
 
 	def start(self):
 		listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		listen_socket.bind(('', self.__port))
 		listen_socket.listen(1)
-		print "SERVER STARTED AT PORT 21" #DEBUG
+		print "SERVER STARTED AT PORT "+str(self.__port) #DEBUG
 		while 1:
 			s, address = listen_socket.accept()
 			print "connection incoming by " +  ', '.join(map(str, address)) #DEBUG
-			s.sendall("120"+'\r\n') #CODICE ERRATO CORREGGERE
+			s.sendall("220"+'\r\n')
 			last_reply = self.handle_request(s)
 			s.sendall(last_reply+'\r\n')
 			s.close()
@@ -41,47 +45,124 @@ class MyFtpServer:
 	def analize_request( self, request ):
 		splitted_request = request.split()
 		if splitted_request[0] not in self.functionality:
-			return "503 Bad sequence of commands."
+			return "500 Command not recognized"
 		else:
 			return self.functionality[splitted_request[0]](self, splitted_request)
 
 
-
 	def user( self, parameter ):
-		if parameter[1] in self.__userdata:
+		if (len(parameter) == 1):
+			return "501 error on parameter number"
+		if parameter[1] in self.__userdata and self.__userConnected == 'EMPTY':
 			self.__userConnected = self.__userdata[parameter[1]]
-			self.__pending_request = 1
-			return "331 Username okay, need password."
+			self.__currentPath = self.__userConnected[1]
+			self.__pending_login = 1
+			return "331 Username okay" #CAMBIARE
+		elif self.__pending_login == 1:
+			return "530 Unexpected reply"
+		elif self.__userConnected != 'EMPTY':
+			return "530"
 		else:
-			return "430 Invalid username or password"
+			return "430 Invalid username or password" #Corretto?
 
-	def pwd(self, parameter):
-		if (self.__userConnected != 'EMPTY') and (self.__userConnected[0] == parameter[1]):
-			self.__pending_request = 0
+	def password(self, parameter):
+		if (len(parameter) == 1):
+			return "501 error on parameter number"
+		if (self.__userConnected != 'EMPTY') and (self.__userConnected[0] == parameter[1]) and self.__pending_login == 1:
+			self.__pending_login = 0
+			try:
+				os.mkdir(self.__currentPath)
+				print "MKDIR: "+self.__currentPath
+			except OSError as HORROR:
+				print "MKDIR: failed"
 			return "230 User logged in, proceed. Logged out if appropriate."
+		elif (self.__pending_login == 0):
+			return "503 Use USER first"
 		else:
-			self.__pending_request = 0
-			return "430 Invalid username or password"
+			self.__pending_login = 0
+			self.__userConnected = 'EMPTY'
+			self.__currentPath = ''
+			return "530 Invalid username or password"
 
 	def feat(self, parameter):
+		if (len(parameter) > 1):
+			return "501 error on parameter number"
+		if self.__pending_login == 1:
+			return "530 Unexpected reply, login incorrect"
 		return "211 CRLF" #EVBB
 
 	def quit(self): #modificare nel caso di upload o download
+		if (len(parameter) > 1):
+			return "501 error on parameter number"
 		self.__userConnected = 'EMPTY'
-		return "221"
+		self.__currentPath = ''
+		return "221 bye bye"
 
+	def syst(self, parameter):
+		if (len(parameter) > 1):
+			return "501 error on parameter number"
+		if self.__pending_login == 1:
+			return "530 Unexpected reply, login incorrect"
+		return "215 UNIX Type: L8 Version"
+
+	def PWD(self, parameter):
+		if (len(parameter) > 1):
+			return "501 error on parameter number"
+		if self.__pending_login == 1:
+			return "530 Unexpected reply, login incorrect"
+		elif (self.is_logged()):
+			return self.__currentPath
+		else:
+			return "530 Please log in with USER and PASS"
+
+	def noop(self, parameter):
+		if (len(parameter) > 1):
+			return "501 error on parameter number"
+		if self.__pending_login == 1:
+			return "530 Unexpected reply, login incorrect"
+		return "200 connection is alive"
+
+	def CWD(self, parameter):
+		if self.__pending_login == 1:
+			return "530 Unexpected repy, login incorrect"
+		if (len(parameter) > 2):
+			return "501 error on parameter number"
+		futurePath = self.__currentPath+parameter[1]
+		if (os.path.exists(futurePath)):
+			self.__currentPath = futurePath
+			return "250 OK, new path is: "+self.__currentPath
+		else:
+			return "550 "+ futurePath +  "doesn't exist"
+
+	def CDUP(self, parameter):
+		if self.__pending_login == 1:
+			return "530 Unexpected repy, login incorrect"
+		if (len(parameter) > 1):
+			return "501 error on parameter number"
+		futurePath = os.path.dirname(os.path.dirname(self.__currentPath))
+		if (futurePath == self.__userConnected[1]):
+			return "250 original path reached"
+		else:
+			self.__currentPath = futurePath
+			return "257 " + self.__currentPath + " is new path"
 
 	functionality = {
 		'USER': user,
-		'PWD': pwd,
-		'FEAT': feat
+		'PASS': password,
+		'FEAT': feat,
+		'SYST': syst,
+		'NOOP': noop,
+		'QUIT': quit,
+		'PWD': PWD,
+		'CWD': CWD,
+		'CDUP': CDUP
 	}
 
 
 if __name__ == '__main__':
 	user_dictionary =  {
-	'admin': ('admin', '/tmp/admin'),
-	'user': ('user', '/tmp/user')
+	'admin': ('admin', '/Users/federico/Desktop/RC/Other/admin'),
+	'user1': ('user1', '/Users/federico/Desktop/RC/Other/user1')
 	}
-	server = MyFtpServer(user_dictionary, 21)
+	server = MyFtpServer(user_dictionary, 210)
 	server.start()
