255ce2e12b0847336960ee00303ed4a6f10caa2b
iskorini <iskorini@gmail.com>
2015-11-08 15:16:27 +0100

    Other function and bugfix

--- server.py
+++ server.py
@@ -2,6 +2,11 @@ import socket
 import os
 import threading
 import random
+import re
+import sys
+import platform
+import subprocess
+import time #CANCELLARE
 class MyFtpServer:
 
 	def __init__ (self, userdata, port):
@@ -13,9 +18,23 @@ class MyFtpServer:
 		self.__ready_to_send = 0
 		self.__address = ''
 		self.__active_connection = 0
+		self.__s = ''
+		self.__type = 'A'
+		self.__random_port = random.randrange(1000, 65536)
+		self.__pasv = 0
+		self.__ip_address = ''
 
 	def is_logged(self):
 		return (self.__userConnected != 'EMPTY' and self.__pending_login == 0)
+
+	def verify_string(self, parameter, regexp):
+		result = re.compile(regexp).match(parameter)
+		if (result == None):
+			return 0
+		elif (result.end() == len(parameter)):
+			return 1
+		else:
+			return 0
 
 	def start(self):
 		listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -23,19 +42,19 @@ class MyFtpServer:
 		listen_socket.listen(1)
 		print "SERVER STARTED AT PORT "+str(self.__port) #DEBUG
 		while 1:
-			s, self.__address = listen_socket.accept()
+			self.__s, self.__address = listen_socket.accept()
 			self.__active_connection = 1
 			print "connection incoming by " +  ', '.join(map(str, self.__address)) #DEBUG
-			s.sendall("220"+'\r\n')
-			self.handle_request(s)
-			s.close()
+			self.__s.sendall("220"+'\r\n')
+			self.handle_request(self.__s)
+			self.__s.close()
 
 	def handle_request(self, sock):
 		while self.__active_connection == 1:
 			request = self.receive_request(sock)
 			print "received message: "+request #DEBUG
 			reply = self.analize_request(request)
-			sock.sendall(reply+'\r\n')
+			sock.sendall(reply)
 
 
 	def receive_request( self , sock ):
@@ -48,29 +67,27 @@ class MyFtpServer:
 	def analize_request( self, request ):
 		splitted_request = request.split()
 		if splitted_request[0] not in self.functionality:
-			return "500 Command not recognized"
+			return "500 Command not recognized\r\n"
+		elif (self.verify_string(request, self.functionality[splitted_request[0]][1]) == 0 ):
+			return "500\r\n"
 		else:
-			return self.functionality[splitted_request[0]](self, splitted_request)
+			return self.functionality[splitted_request[0]][0](self, request)
 
 
 	def user( self, parameter ):
-		if (len(parameter) == 1):
-			return "501 error on parameter number"
-		if parameter[1] in self.__userdata and self.__userConnected == 'EMPTY':
-			self.__userConnected = self.__userdata[parameter[1]]
+		if (self.is_logged() == 1 and parameter[5:(len(parameter)-2)] == self.__userConnected):
+			return "331 user is logged\r\n"
+		elif (parameter[5:(len(parameter)-2)] in self.__userdata and self.__userConnected == 'EMPTY'):
+			self.__userConnected = self.__userdata[parameter[5:(len(parameter)-2)]]
 			self.__pending_login = 1
-			return "331 Username okay" #CAMBIARE
-		elif self.__pending_login == 1:
-			return "530 Unexpected reply"
-		elif self.__userConnected != 'EMPTY':
-			return "530"
+			return "331 Username okay\r\n" #CAMBIARE
+		elif (self.__userConnected != 'EMPTY'):
+			return "530\r\n"
 		else:
-			return "430 Invalid username or password" #Corretto?
+			return "430 Invalid username or password\r\n" #Corretto?
 
 	def password(self, parameter):
-		if (len(parameter) == 1):
-			return "501 error on parameter number"
-		if (self.__userConnected != 'EMPTY') and (self.__userConnected[0] == parameter[1]) and self.__pending_login == 1:
+		if (self.__userConnected != 'EMPTY') and (self.__userConnected[0] == parameter[5:(len(parameter)-2)]) and self.__pending_login == 1:
 			self.__pending_login = 0
 			try:
 				os.mkdir(self.__userConnected[1])
@@ -78,121 +95,156 @@ class MyFtpServer:
 			except OSError as HORROR:
 				print "MKDIR: failed"
 			os.chdir(self.__userConnected[1])
-			return "230 User logged in, proceed. Logged out if appropriate."
+			return "230 User logged in, proceed. Logged out if appropriate.\r\n"
+		elif (self.is_logged == 1):
+			return "230 user is already logged\r\n"
 		elif (self.__pending_login == 0):
-			return "503 Use USER first"
+			return "503 Use USER first\r\n"
 		else:
 			self.__pending_login = 0
 			self.__userConnected = 'EMPTY'
-			return "530 Invalid username or password"
+			return "530 Invalid username or password\r\n"
 
 	def feat(self, parameter):
-		if (len(parameter) > 1):
-			return "501 error on parameter number"
+		if (self.is_logged() == 0):
+			return "530 please log in\r\n"
 		if self.__pending_login == 1:
-			return "530 Unexpected reply, login incorrect"
-		return "211 CRLF" #EVBB
+			return "331 Unexpected reply, login incorrect\r\n"
+		return "211 \r\n" #EVBB
 
 	def quit(self, parameter): #modificare nel caso di upload o download
-		if (len(parameter) > 1):
-			return "501 error on parameter number"
 		self.__userConnected = 'EMPTY'
 		self.__active_connection = 0
-		return "221 bye bye"
+		return "221 bye bye\r\n"
 
 	def syst(self, parameter):
-		if (len(parameter) > 1):
-			return "501 error on parameter number"
+		if (self.is_logged() == 0):
+			return "530 please log in\r\n"
 		if self.__pending_login == 1:
-			return "530 Unexpected reply, login incorrect"
-		return "215 UNIX Type: L8 Version"
+			return "331 Unexpected reply, login incorrect\r\n"
+		return "215 "+self.system_info()+"\r\n"
 
 	def PWD(self, parameter):
-		if (len(parameter) > 1):
-			return "501 error on parameter number"
 		if self.__pending_login == 1:
-			return "530 Unexpected reply, login incorrect"
+			return "331 Unexpected reply, login incorrect\r\n"
 		elif (self.is_logged()):
-			return "257 current directory is: "+os.getcwd()
+			return "257 current directory is: "+os.getcwd()+"\r\n"
 		else:
-			return "530 Please log in with USER and PASS"
+			return "530 Please log in with USER and PASS\r\n"
 
 	def noop(self, parameter):
-		if (len(parameter) > 1):
-			return "501 error on parameter number"
+		if (self.is_logged() == 0):
+			return "530 please log in\r\n"
 		if self.__pending_login == 1:
-			return "530 Unexpected reply, login incorrect"
-		return "200 connection is alive"
+			return "331 Unexpected reply, login incorrect\r\n"
+		return "200 connection is alive\r\n"
 
 	def CWD(self, parameter):
 		if self.__pending_login == 1:
-			return "530 Unexpected repy, login incorrect"
-		if (len(parameter) != 2):
-			return "501 error on parameter number"
+			return "331 Unexpected repy, login incorrect\r\n"
 		try:
 			os.chdir(parameter[1])
-			return "250 OK, new path is: " + os.getcwd()
+			return "250 OK, new path is: " + os.getcwd()+"\r\n"
 		except:
-			return "550 "+ parameter[1] +  " doesn't exist"
+			return "550 "+ parameter[1] +  " doesn't exist\r\n"
 
 	def CDUP(self, parameter):
 		if self.__pending_login == 1:
-			return "530 Unexpected reply, login incorrect"
-		if (len(parameter) > 1):
-			return "501 error on parameter number"
+			return "331 Unexpected reply, login incorrect\r\n"
 		if (os.getcwd() == self.__userConnected[1]):
-			return "250 original path reached"
+			return "250 original path reached\r\n"
 		else:
 			os.chdir(os.path.dirname(os.getcwd()))
-			return "257 " + os.getcwd() + " is new path"
+			return "257 " + os.getcwd() + " is new path\r\n"
 
 	def port(self, parameter):
 		if (self.is_logged() == 0):
-			return "530 Please log in with USER and PASS"
+			return "530 Please log in with USER and PASS\r\n"
 		if (self.__pending_login == 1):
-			return "530 Unexpected reply, login incorrect"
-		if (len(parameter)!=7):
-			return "501 error on parameter number"
-		new_parameter = map(lambda x: x.replace(",", ""), parameter)
-		ip_address = []
-		for i in range(1,5):
-			ip_address.append(new_parameter[i])
-		ip_address = reduce(lambda x, y: str(x)+"."+str(y), ip_address)
-		port = int(parameter[5])*256+int(parameter[6])*1 #bah
-		self.__data_conn = data_transfer(ip_address, port, 0)
-		self.__data_conn.start()
+			return "331 Unexpected reply, login incorrect\r\n"
+		transfer_data = parameter[5:].split(",")
+		self.__ip_address = transfer_data[0:4]
+		self.__ip_address = reduce(lambda x, y: str(x)+"."+str(y), ip_address)
+		self.__random_port = int(transfer_data[4])*256+int(transfer_data[5])*1 #bah
 		self.__ready_to_send = 1
-		return "200 success"
+		return "200 success\r\n"
 
 	def pasv(self, parameter):
 		if (self.__pending_login == 1):
-			return "530 Unexpected reply, login incorrect"
-		if (len(parameter)!=1):
-			return "501 error on parameter number"
+			return "331 Unexpected reply, login incorrect\r\n"
 		if (self.is_logged() == 0):
-			return "530 Please log in with USER and PASS"
-		port = random.randrange(1000, 65536)
+			return "530 Please log in with USER and PASS\r\n"
+		port = self.__random_port
 		x = port/256 #BAH
 		y = port-(x*256)
-		self.__data_conn = data_transfer(self.__address[0], port ,1)
-		self.__data_conn.start()
+		self.__pasv = 1
 		self.__ready_to_send = 1
 		print "PASSIVE MODE ON PORT: "+str(port)
-		return "227 entering in passive mode ("+self.__address[0].replace(".",",")+","+str(x)+","+str(y)+")"
+		return "227 entering in passive mode ("+self.__address[0].replace(".",",")+","+str(x)+","+str(y)+")"+"\r\n"
+
+
+	def send_list(self, parameter): #gestire casi di errore
+		if (self.is_logged() == 0):
+			return "530 Please log in with USER and PASS\r\n"
+		if (self.__pending_login == 1):
+			return "331 Unexpected reply, login incorrect\r\n"
+		if self.__ready_to_send == 1:
+			if (self.__pasv == 1):
+				data_conn = data_transfer('', self.__random_port ,1) #self.__address[0]
+			else:
+				self.__data_conn = data_transfer(ip_address, self.__random_port, 0)
+		data_conn.start() #avvio il thread
+		self.__s.sendall('150 Opening ASCII mode data connection for \'/bin/ls\'\r\n') #MANDO
+		l = parameter.split()
+		if (len(l) != 1):
+		    ls_comand = 'ls '+ reduce(lambda x, y: str(x)+" "+str(y), l[1:])
+		else:
+		    ls_comand = 'ls'
+		data_to_transfer = (subprocess.check_output(ls_comand, shell = True))
+		if (self.__type = 'A'):
+			data_to_transfer.replace('\n','\r\n')
+		data_conn.send_data(data_to_transfer)
+		data_conn.join() #TERMINO IL THREAD
+		return "226 transfer complete\r\n"
+
+	def set_type(self, parameter):
+		if (self.is_logged() == 0):
+			return "530 please log in\r\n"
+		if self.__pending_login == 1:
+			return "331 Unexpected reply, login incorrect\r\n"
+
+
+	def retr(self, parameter):
+		if (self.is_logged() == 0):
+			return "530 please log in\r\n"
+		if self.__pending_login == 1:
+			return "331 Unexpected reply, login incorrect\r\n"
+
+	def system_info(self):
+		plat = sys.platform
+		if plat == 'darwin':
+			return "OS X "+str(platform.mac_ver()[0])+"\r\n"
+		elif plat == 'win32':
+			return "Windows "+str(platform.version())+"\r\n"
+		elif plat == 'linux' or plat == 'linux2':
+			return "LINUX " +str(platform.version())+"\r\n"
 
 
 	functionality = {
-		'USER': user,
-		'PASS': password,
-		'FEAT': feat,
-		'SYST': syst,
-		'NOOP': noop,
-		'QUIT': quit,
-		'PWD': PWD,
-		'CWD': CWD,
-		'CDUP': CDUP,
-		'PORT': port,
-		'PASV': pasv
+		'USER': (user, '\AUSER\s(\w|\s)+$'),
+		'PASS': (password, '\APASS\s(\w|\s)+$'),
+		'FEAT': (feat, '\AFEAT\\r\\n$'),
+		'SYST': (syst, '\ASYST\\r\\n$'),
+		'NOOP': (noop, '\ANOOP\\r\\n$'),
+		'QUIT': (quit, '\AQUIT\\r\\n$'),
+		'PWD': (PWD, '\APWD\\r\\n$'),
+		'CWD': (CWD, '^CWD\s(.*/)?(?:$|(.+?)(?:(\.[^.]*$)|$))\\r\\n$'),
+		'CDUP': (CDUP, '\ACDUP\\r\\n$'),
+		'PORT': (port, '^PORT\s\d{1,3},\d{1,3},\d{1,3},\d{1,3},\d{1,3},\d{1,3}\\r\\n$'),
+		'PASV': (pasv, '\APASV\\r\\n$'),
+		'LIST': (send_list, '^LIST(\s)?((-l|-la)(\s)?)?((.*/)?(?:$|(.+?)(?:(\.[^.]*$)|$)))?\\r\\n$'),
+		'TYPE': (set_type, '\ATYPE\s(A|I)\\r\\n$'),
+		'RETR': (retr, '^RETR\s(.*/)?(?:$|(.+?)(?:(\.[^.]*$)|$))\\r\\n$')
 	}
 
 class data_transfer(threading.Thread): #classe per trasferimenti dati
@@ -201,6 +253,8 @@ class data_transfer(threading.Thread): #classe per trasferimenti dati
 		self.__ap = (address, port)
 		self.__sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		self.__pasv = pasv
+		self.__semaphore = threading.Semaphore(0)
+		self.__s = ''
 
 	def run(self):
 		if (self.__pasv == 1): #Sono in PASV
@@ -211,17 +265,24 @@ class data_transfer(threading.Thread): #classe per trasferimenti dati
 		elif (self.__pasv == 0): #Sono in attivo
 			self.__sock.connect(self.__ap)
 			print "DATA CONNECTION IS ALIVE"
-
+		self.__semaphore.release()
 	def send_data(self, data):
-		self.__s.sendall(data)
-		self.__s.close()
+		self.__semaphore.acquire()
+		if (self.__pasv == 1):
+			self.__s.sendall(data)
+			self.__s.close()
+		else:
+			self.__sock.sendall(data)
+			self.__sock.close()
+
 
 
 
 if __name__ == '__main__':
 	user_dictionary =  {
 	'admin': ('admin', '/Users/federico/Desktop/RC/Other/admin'),
-	'user1': ('user1', '/Users/federico/Desktop/RC/Other/user1')
+	'user1': ('user1', '/Users/federico/Desktop/RC/Other/user1'),
+	'user 666':('user 666', '/Users/federico/Desktop/RC/Other/user 666')
 	}
 	server = MyFtpServer(user_dictionary, 210)
 	server.start()
--- test.py
+++ test.py
@@ -1,2 +1,59 @@
-address = "127.0.0.1"
-print address.replace(".",",")
+import os
+import threading
+import socket
+string = 'USER admin\r\n'
+username = string[5:]
+print username
+user_dictionary =  {
+'admin': ('admin', '/Users/federico/Desktop/RC/Other/admin'),
+'user1': ('user1', '/Users/federico/Desktop/RC/Other/user1')
+}
+print user_dictionary[string[5:len(string)-2]]
+print string[5:len(string)-2] in user_dictionary
+
+print "------------------------------------------------"
+
+test1 = "PORT 2,124,54,1,4,1"
+transfer_data = test1[5:].split(",")
+ip_address = transfer_data[0:4]
+ip_address = reduce(lambda x, y: str(x)+"."+str(y), ip_address)
+print ip_address
+port = int(transfer_data[4])*256+int(transfer_data[5])*1
+print port
+
+print "---------------------------------"
+x = os.system("ls -la")
+print str(x)
+
+print "----------------------------------"
+semaphore = threading.Semaphore(1)
+
+semaphore.acquire()
+print("DIOMERDA")
+
+print "--------------------"
+
+print os.listdir(os.getcwd())
+
+print "----------------------"
+
+#list0 = "LIST"
+#list0 = "LIST -la"
+list0 = "LIST -l /Users/Federico/blabla"
+print list0
+l = list0.split()
+if (len(list0) != 4):
+    ls_comand = 'ls '+ reduce(lambda x, y: str(x)+" "+str(y), l[1:])
+else:
+    ls_comand = 'ls'
+print ls_comand
+
+print "-----------------"
+socket1 = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+socket2 = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+socket1.bind(('', 9000))
+socket2.bind(('', 9001))
+socket1.listen(1)
+socket2.listen(1)
+socket1.accept()
+socket2.accept()
